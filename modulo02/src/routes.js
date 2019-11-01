1c591d409dbe9cf579d23eb2456ebe86264559e3
Benones Alves <benonesone@gmail.com>
2019-10-31 22:31:32 -0300

    Gravacao BD

--- modulo02/src/routes.js
+++ modulo02/src/routes.js
@@ -1,7 +1,17 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Olá Paus' }));
+routes.get('/', async (req, res) => {
+    const user = await User.create({
+        name: 'Ben',
+        email: 'ben@ben',
+        password_hash: '123456789',
+        provider: true
+    });
+
+    return res.json(user);
+});
 
 export default routes;
